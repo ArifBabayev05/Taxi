16670789c110daa6543ff0b8adb5502599a5cb95
ArifBabayev05 <101328505+ArifBabayev05@users.noreply.github.com>
2022-03-17 00:45:43 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace dotnet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = 10;
+            double distance = 1;
+            double pay;
+
+            if (distance>5)
+            {
+                pay = distance * 0.7;
+            }
+            else
+            {
+                pay = distance * 1;
+            }
+            if (balance>pay)
+
+            {
+                Console.WriteLine("Sifariş Uğurludur.");
+            }
+            else
+            {
+                Console.WriteLine("Sifariş Uğursuzdur.");
+            }
+        }
+    }
+}
